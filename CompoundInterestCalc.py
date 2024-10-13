a43dbfdcf35ee4d1009b9d2548dcccb7f9b72791
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 04:57:40 -0400

    added

--- CompoundInterestCalc.py
+++ CompoundInterestCalc.py
@@ -6,14 +6,14 @@ time = 0
 
 while True:
     principle = float(input("enter principle amount: "))
-    if principle < 0:
+    if principle <= 0:
         print("principle cannot be zero!")
     else:
         break
 
 while True:
     rate = float(input("enter interest rate: "))
-    if rate < 0:
+    if rate <= 0:
         print("Interest rate cannot be less than or equal to zero!")
     else:
         break
