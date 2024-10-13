ddbbc712f483e2c4c388b47e872b7cc3ddc3ffd8
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 04:39:49 -0400

    added

--- CompoundInterestCalc.py
+++ CompoundInterestCalc.py
@@ -1,1 +1,23 @@
 #CompoundInterestCalc
+
+principle = 0
+rate = 0
+time = 0
+
+while True:
+    principle = float(input("enter principle amount: "))
+    if principle < 0:
+        print("principle cannot be zero!")
+
+while True:
+    rate = float(input("enter interest rate: "))
+    if rate < 0:
+        print("Interest rate cannot be less than or equal to zero!")
+
+while True:
+    time = float(input("enter investment period in years: "))
+    if time <= 0:
+        print("time cannot be less than or equal to zero!")
+    
+total = round(principle * pow((1 + rate / 100), time), 2)
+print(f" the total amount after {time} years with a principle of ${principle} is ${total}")
