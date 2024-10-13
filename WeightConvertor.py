f198bb8b901cf36e3c14beca5b1f44f3ef7a5516
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 02:56:46 -0400

    add

--- WeightConvertor.py
+++ WeightConvertor.py
@@ -1,13 +1,14 @@
 #WeightConvertor
 
+print("weight Convertor")
 Weight = float(input("how much do you weigh?: "))
 unit = input("Kgs or Lbs?: ").lower()
 
-if unit == "Kgs":
+if unit == "kgs":
     WeightLbs = round(Weight * 2.205, 2)
-    print(f" you weight is {WeightLbs} in Lbs")
-elif unit == "Lbs":
+    print(f" you weight in lbs is {WeightLbs}")
+elif unit == "lbs":
     WeightKgs = round(Weight / 2.205, 2)
-    print(f" your weight is {WeightKgs} in Kgs")
+    print(f" your weight in Kgs is {WeightKgs}")
 else:
     print(f"{unit} is invalid")
