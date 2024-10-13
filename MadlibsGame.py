365d937f94d14a05d12b18db2efbc491ce9744c9
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 02:13:58 -0400

    added

--- MadlibsGame.py
+++ MadlibsGame.py
@@ -12,11 +12,5 @@ Name = input("give me a name")
 print("1. Space Adventure")
 print(f"A {Adjective1} astronaut flew to {Noun1} in their {Noun2}. They met a {Adjective2} alien and gave them {Pluralnoun1}.")
 
-print("2. The Talking Tree")
-#In a [adjective] forest, a tree could [verb]. One day, [name] asked the tree for a [noun].
-
-print("3. Pizza Party")
-#I had a [adjective] pizza party with [number] pizzas. We ate [plural noun] and played [game].
-
-print("5. Superhero")
+print("2. Superhero")
 #[Name] was a superhero who could [verb]. They saved the city from a [noun] and became a [adjective] hero.
