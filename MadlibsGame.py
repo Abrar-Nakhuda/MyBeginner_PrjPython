d31496331aaf9b95b5952eda576d897ac3604ecf
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 02:11:44 -0400

    added

--- MadlibsGame.py
+++ MadlibsGame.py
@@ -1,23 +1,22 @@
 #MadlibsGame
 
-Adjective1 = input("Describe something: ")
-Noun1 = input( "tell me about A person, place, or thing: ")
+Adjective1 = input ( "Describe something: ")
+Adjective2 = input ( "Describe something else: ")
+Noun1 = input( "tell me about a person, place, or thing: ")
+Noun2 = input( "tell me about another person, place, or thing: ")
 Verb1 = input("An action word": )
 Adverb1 = input("How something is done: " )
 Pluralnoun1 = input("Multiple items: ")
 Name = input("give me a name")
 
 print("1. Space Adventure")
-A [adjective] astronaut flew to [place] in their [noun]. They met a [adjective] alien and gave them [plural noun].
+print(f"A {Adjective1} astronaut flew to {Noun1} in their {Noun2}. They met a {Adjective2} alien and gave them {Pluralnoun1}.")
 
 print("2. The Talking Tree")
-In a [adjective] forest, a tree could [verb]. One day, [name] asked the tree for a [noun].
+#In a [adjective] forest, a tree could [verb]. One day, [name] asked the tree for a [noun].
 
 print("3. Pizza Party")
-I had a [adjective] pizza party with [number] pizzas. We ate [plural noun] and played [game].
-
-print("4. Haunted House")
-I went into a [adjective] haunted house. I saw a [adjective] ghost and ran out with a [noun].
+#I had a [adjective] pizza party with [number] pizzas. We ate [plural noun] and played [game].
 
 print("5. Superhero")
-[Name] was a superhero who could [verb]. They saved the city from a [noun] and became a [adjective] hero.
+#[Name] was a superhero who could [verb]. They saved the city from a [noun] and became a [adjective] hero.
