4daa47e2ec0a7ec544824e3acb865382b1161ab2
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 02:14:49 -0400

    added

--- MadlibsGame.py
+++ MadlibsGame.py
@@ -4,8 +4,7 @@ Adjective1 = input ( "Describe something: ")
 Adjective2 = input ( "Describe something else: ")
 Noun1 = input( "tell me about a person, place, or thing: ")
 Noun2 = input( "tell me about another person, place, or thing: ")
-Verb1 = input("An action word": )
-Adverb1 = input("How something is done: " )
+Verb1 = input("An action word:"  )
 Pluralnoun1 = input("Multiple items: ")
 Name = input("give me a name")
 
@@ -13,4 +12,4 @@ print("1. Space Adventure")
 print(f"A {Adjective1} astronaut flew to {Noun1} in their {Noun2}. They met a {Adjective2} alien and gave them {Pluralnoun1}.")
 
 print("2. Superhero")
-#[Name] was a superhero who could [verb]. They saved the city from a [noun] and became a [adjective] hero.
+print(f" {Name} was a superhero who could {Verb1}. They saved the city from a {Noun2} and became a {Adjective1} hero.")
