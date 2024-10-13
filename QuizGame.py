b4aa36ad629f6292e5c96d57a2066d57c99ec1e5
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 19:00:06 -0400

    added

--- QuizGame.py
+++ QuizGame.py
@@ -10,10 +10,42 @@ options = (("A. Berlin", "B. Madrid", "C. Rome", "D. Paris"),
            ("A. Venus ", "B. Mars ", "C. Jupiter ", "D. Saturn"),
            ("A. Charles Dickens", "B. William Shakespeare", "C. Jane Austen", "D. Mark Twain"),
            ("A. Ag", "B. Au", "C. Fe", "D. Pb"),
-           (" ", " ", " ", " "),
            ("A. China", "B. South Korea", "C. Japan", "D. Thailand"))
 
-answers = ()
+answers = ("D", "B","B", "B", "C")
 guesses = []
 score = 0
 question_num = 0
+
+for question in questions:
+    print("------------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Choose an option (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("Correct!")
+    else:
+        print("Incorrect!")
+        print(f"{answers[question_num]} is the correct answer!")
+    question_num += 1 # this will increment the question numbers
+
+print("------------------------------")
+print("          RESULTS             ")
+print("------------------------------")
+print("answers: ", end=" " )
+for answer in answers:
+    print(answer, end=" ") # end=" " in the print() function prints answers on the same line with spaces between them.
+print() # The extra print() ensures that after printing the answers, the cursor moves to the next line, making the output more readable.
+
+print("guesses: ", end=" " )
+for guess in guesses:
+    print(guess, end=" ")
+print()
+
+score = int(score / len(questions) * 100) 
+print(f" your total score is {score}")
+print()
