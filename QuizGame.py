e917f5e203cafe8c5df0cf8daf3e4a765d2345db
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 18:20:00 -0400

    add

--- QuizGame.py
+++ QuizGame.py
@@ -3,10 +3,15 @@
 questions = ("What is the capital city of France?: ",
             "Which planet is known as the 'Red Planet'?: ",
             "Who wrote the play 'Romeo and Juliet'?:  ",
-            " ",
-            " ")
+            "What is the chemical symbol for gold?: ",
+            "Which country is known as the Land of the Rising Sun?: ")
 
-options = ((), (), (), (), (), ())
+options = (("A. Berlin", "B. Madrid", "C. Rome", "D. Paris"),
+           ("A. Venus ", "B. Mars ", "C. Jupiter ", "D. Saturn"),
+           ("A. Charles Dickens", "B. William Shakespeare", "C. Jane Austen", "D. Mark Twain"),
+           ("A. Ag", "B. Au", "C. Fe", "D. Pb"),
+           (" ", " ", " ", " "),
+           ("A. China", "B. South Korea", "C. Japan", "D. Thailand"))
 
 answers = ()
 guesses = []
