96c9f8e52dc60734e73540d70c1081af1a3bab9a
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 01:23:36 -0400

    added my calculator

---
+++ simple_calculator.py
@@ -0,0 +1,31 @@
+#simple_calculator
+
+operator = input("choose an  operator( + - / * % // **): ") # this will decide the operation
+
+num1 = int(input("choose your first number: ")) 
+num2 = int(input("choose your second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} equals {result}")
+elif operator == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} equals {result}")
+elif operator == "/":
+    result = num1 / num2
+    print(f"{num1} / {num2} equals {result}")
+elif operator == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} equals {result}")
+elif operator == "%":
+    result = num1 % num2
+    print(f"{num1} % {num2} equals {result}")
+elif operator == "//":
+    result = num1 // num2
+    print(f"{num1} // {num2} equals {result}")
+elif operator == "**":
+    result = num1 ** num2
+    print(f"{num1} ** {num2} equals {result}")
+else:
+    
+    print(f" {operator} is an invalid operator")
