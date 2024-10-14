52f07969ea07ed3bd05f57d6d5e3783642827b14
Abrar <abrar.nakhuda85@gmail.com>
2024-10-13 21:20:11 -0400

    added

--- RockPapersScissorsGame.py
+++ RockPapersScissorsGame.py
@@ -4,6 +4,8 @@ import random
 
 options = ("rock", "paper", "scissors")
 running = True
+player_score = 0
+computer_score = 0
 
 while running:
     
@@ -12,6 +14,7 @@ while running:
 
     while player not in options:
         player = input("enter a choice (rock, paper, scissors): ")
+
         print(f"Player: {player}")
         print(f"Computer: {computer}")
 
@@ -19,14 +22,19 @@ while running:
         print("its a tie!")
     elif player == "rock" and computer == "scissors":
         print(f"player wins! {player} beats {computer}!!")
+        player_score += 1
     elif player == "paper" and computer == "rock":
         print(f"player wins! {player} beats {computer}!!")
+        player_score += 1
     elif player == "scissors" and computer == "paper":
-        print(f"player wins! {player} beats {computer}!!")  
+        print(f"player wins! {player} beats {computer}!!")
+        player_score += 1  
     else:
         print(f"Computer wins! {computer} beats {player}")
+        computer_score += 1
 
     if not input("Play again? (y/n): ").lower() == "y":
         running = False
 
 print ("thanks for playing!")
+print(f"Final Scores:\nPlayer: {player_score}\nComputer: {computer_score}")
