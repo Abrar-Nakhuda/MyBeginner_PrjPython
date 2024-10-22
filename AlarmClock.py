673d7f38dd49d178bcb5197be94118edc457109c
Abrar <abrar.nakhuda85@gmail.com>
2024-10-22 13:13:56 -0400

    h

--- AlarmClock.py
+++ AlarmClock.py
@@ -1,1 +1,12 @@
-AlarmClock
+#AlarmClock
+import time # we'll be updating the time every second
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+
+if __name__ == "__main__":
+    alarm_time = input("enter the alarm time (HH:MM:SS)")
+    set_alarm(alarm_time)
+
