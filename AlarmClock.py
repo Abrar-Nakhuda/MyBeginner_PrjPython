6e80cd3b5fbe81ead4124a37a62642b3c0434919
Abrar <abrar.nakhuda85@gmail.com>
2024-10-22 13:19:50 -0400

    add

--- AlarmClock.py
+++ AlarmClock.py
@@ -5,6 +5,7 @@ import pygame
 
 def set_alarm(alarm_time):
     print(f"Alarm set for {alarm_time}")
+    sound_file = "AlarmClockSound.mps"
 
 if __name__ == "__main__":
     alarm_time = input("enter the alarm time (HH:MM:SS)")
